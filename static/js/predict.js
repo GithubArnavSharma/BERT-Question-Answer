2423fcbec1160821b251d9570dc832cd827bb433
GithubArnavSharma <77365987+GithubArnavSharma@users.noreply.github.com>
2021-05-23 11:28:33 -0700

    Create predict.js

---
+++ static/js/predict.js
@@ -0,0 +1,24 @@
+document.getElementById("predict-button").addEventListener("click", function() {
+    let question = document.getElementById('quest');
+    let paragraph = document.getElementById('para');
+    let result = document.getElementById('result-text');
+
+    const getPrediction = async (question, paragraph) => {
+       const response = await fetch('/predict', {
+            method: 'POST',
+            body: JSON.stringify({'question':question, 'paragraph':paragraph}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+       });
+       const myJson = await response.json();
+            return myJson.result;
+       }
+
+   let predtext = getPrediction(question.value, paragraph.value);
+       predtext.then(function (response) {
+            result.textContent = response;
+    });
+
+
+}, false);
