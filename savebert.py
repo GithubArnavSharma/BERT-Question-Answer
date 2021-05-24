5e2e602397f4f3ffbb328bb28817233102d3d0dd
GithubArnavSharma <77365987+GithubArnavSharma@users.noreply.github.com>
2021-05-23 22:51:07 -0700

    Create savebert.py

---
+++ savebert.py
@@ -0,0 +1,9 @@
+import pickle
+from transformers import BertForQuestionAnswering
+from transformers import BertTokenizer
+
+model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+
+pickle.dump(model, open('QAModel', 'wb'))
+pickle.dump(tokenizer, open('tokenizer', 'wb'))
